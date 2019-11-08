3ed77c8a708c0ea76b48cbb8e44ce5a5a76d174a
SOO RUI QI <TP041814@mail.apu.edu.my>
2019-11-08 15:08:41 +0800

    Add project files.

---
+++ TestUserGidHubFORDeployment/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace TestUserGidHubFORDeployment.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
